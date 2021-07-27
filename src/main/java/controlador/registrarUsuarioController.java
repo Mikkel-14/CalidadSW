8d9a3e17a0e6b5e48b8de16859debdbc687d2881
Santiago <santiago.villegas@epn.edu.ec>
2021-07-27 13:25:11 -0500

    Adición comprobaciones y dirección al login

--- src/main/java/controlador/registrarUsuarioController.java
+++ src/main/java/controlador/registrarUsuarioController.java
@@ -29,35 +29,53 @@ public class registrarUsuarioController extends HttpServlet {
         String password = req.getParameter("password");
         String rol = req.getParameter("rol");
         DAOFactory fabrica = new JPAFactory();
-        if (rol.equals("administrador")){
-            Administrador administradorConsutado = (Administrador) fabrica.crearUsuarioDAO(JPAFactory.ADMINISTRADOR).leer(codigoUnico);
-            if(administradorConsutado == null){
-                HashPassword hash = new HashPassword();
-                String salt = HashPassword.getSalt();
-                String saltedPasswd = hash.valorHash(password+salt);
-                Administrador administrador = new Administrador(codigoUnico,nombre,apellido,saltedPasswd,email);
-                administrador.setSal(salt);
-                fabrica.crearUsuarioDAO(JPAFactory.ADMINISTRADOR).crear(administrador);
-                req.setAttribute("mensajeExito", "Se ha registrado el Administrador");//mensaje
-            }else{
-                req.setAttribute("mensajeError", "Administrador ya registrado");//mensaje
+        if(Validaciones.validadorCodUnico(codigoUnico)
+                & Validaciones.validadorCorreo(email)
+                & Validaciones.validadorStrings(nombre)
+                & Validaciones.validadorStrings(apellido)) {
+            if (rol.equals("administrador")) {
+                Administrador administradorConsutado = (Administrador) fabrica.crearUsuarioDAO(JPAFactory.ADMINISTRADOR).leer(codigoUnico);
+                if (administradorConsutado == null) {
+                    HashPassword hash = new HashPassword();
+                    String salt = HashPassword.getSalt();
+                    String saltedPasswd = hash.valorHash(password + salt);
+                    Administrador administrador = new Administrador(codigoUnico, nombre, apellido, saltedPasswd, email);
+                    administrador.setSal(salt);
+                    fabrica.crearUsuarioDAO(JPAFactory.ADMINISTRADOR).crear(administrador);
+                    req.setAttribute("mensajeExito", "Se ha registrado el Administrador");//mensaje
+                    getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+                } else {
+
+                    //getServletContext().getRequestDispatcher("/registroUsuario.jsp").forward(req, resp);
+                }
+
+            } else if (rol.equals("usuario")) {
+                Usuario usuarioConsutado = (Usuario) fabrica.crearUsuarioDAO(JPAFactory.USUARIO).leer(codigoUnico);
+                if (usuarioConsutado == null) {
+                    HashPassword hash = new HashPassword();
+                    String salt = HashPassword.getSalt();
+                    String saltedPasswd = hash.valorHash(password + salt);
+                    Usuario usuario = new Usuario(codigoUnico, nombre, apellido, saltedPasswd, email);
+                    usuario.setSal(salt);
+                    fabrica.crearUsuarioDAO(JPAFactory.ADMINISTRADOR).crear(usuario);
+
+                    //getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+                } else {
+                    req.setAttribute("mensajeError", "Usuario ya registrado");//mensaje
+                    getServletContext().getRequestDispatcher("/registroUsuario.jsp").forward(req, resp);
+                }
             }
 
-        }else if (rol.equals("usuario")){
-            Usuario usuarioConsutado = (Usuario) fabrica.crearUsuarioDAO(JPAFactory.USUARIO).leer(codigoUnico);
-            if (usuarioConsutado == null){
-                HashPassword hash = new HashPassword();
-                String salt = HashPassword.getSalt();
-                String saltedPasswd = hash.valorHash(password+salt);
-                Usuario usuario = new Usuario(codigoUnico,nombre,apellido,saltedPasswd,email);
-                usuario.setSal(salt);
-                fabrica.crearUsuarioDAO(JPAFactory.ADMINISTRADOR).crear(usuario);
-                req.setAttribute("mensajeExito", "Se ha registrado el Usuario");//mensaje
-            }else{
-                req.setAttribute("mensajeError", "Usuario ya registrado");//mensaje
-            }
-
+        }else{
+            req.setAttribute("codigoUnico",codigoUnico);
+            req.setAttribute("nombre",nombre);
+            req.setAttribute("apellido",apellido);
+            req.setAttribute("email",email);
+            req.setAttribute("password",password);
+            req.setAttribute("rol",rol);
+            req.setAttribute("mensajeError", "Datos no válidos");//mensaje
+            getServletContext().getRequestDispatcher("/registroUsuario.jsp").forward(req, resp);
         }
-        getServletContext().getRequestDispatcher("/registroUsuario.jsp").forward(req, resp);
+
     }
 }
